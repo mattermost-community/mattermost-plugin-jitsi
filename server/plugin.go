2c62318241a81b71cd7f7198356b1122854bece6
mkadarik <kadarik1@gmail.com>
2020-03-21 19:17:09 +0200

    Add JWT support

--- server/plugin.go
+++ server/plugin.go
@@ -5,10 +5,13 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"net/url"
 	"regexp"
 	"strings"
 	"sync"
+	"time"
 
+	"github.com/cristalhq/jwt"
 	"github.com/mattermost/mattermost-server/model"
 	"github.com/mattermost/mattermost-server/plugin"
 )
@@ -51,6 +54,17 @@ type StartMeetingRequest struct {
 	Personal  bool   `json:"personal"`
 	Topic     string `json:"topic"`
 	MeetingId int    `json:"meeting_id"`
+}
+
+// Claims extents cristalhq/jwt standard claims to add jitsi-web-token specific fields
+type Claims struct {
+	jwt.StandardClaims
+	Room string `json:"room,omitempty"`
+}
+
+// MarshalBinary default marshaling to JSON.
+func (c Claims) MarshalBinary() (data []byte, err error) {
+	return json.Marshal(c)
 }
 
 func encodeJitsiMeetingID(meeting string) string {
@@ -101,32 +115,67 @@ func (p *Plugin) handleStartMeeting(w http.ResponseWriter, r *http.Request) {
 	jitsiURL = strings.TrimRight(jitsiURL, "/")
 	meetingURL := jitsiURL + "/" + meetingID
 
+	var meetingLinkValidUntil = time.Time{}
+	JWTMeeting := p.getConfiguration().JitsiJWT
+
+	if JWTMeeting {
+		signer, err2 := jwt.NewHS256([]byte(p.getConfiguration().JitsiAppSecret))
+		if err2 != nil {
+			log.Printf("Error generating new HS256 signer: %v", err2)
+			http.Error(w, "Internal error", http.StatusInternalServerError)
+			return
+		}
+		builder := jwt.NewTokenBuilder(signer)
+
+		// Error check is done in configuration.IsValid()
+		jURL, _ := url.Parse(p.getConfiguration().JitsiURL)
+
+		meetingLinkValidUntil = time.Now().Add(time.Duration(p.getConfiguration().JitsiLinkValidTime) * time.Minute)
+
+		claims := Claims{}
+		claims.Issuer = p.getConfiguration().JitsiAppID
+		claims.Audience = []string{p.getConfiguration().JitsiAppID}
+		claims.ExpiresAt = jwt.Timestamp(meetingLinkValidUntil.Unix())
+		claims.Subject = jURL.Hostname()
+		claims.Room = meetingID
+
+		token, err2 := builder.Build(claims)
+		if err2 != nil {
+			log.Printf("Error building JWT: %v", err2)
+			http.Error(w, "Internal error", http.StatusInternalServerError)
+			return
+		}
+
+		meetingURL = meetingURL + "?jwt=" + string(token.Raw())
+	}
+
 	post := &model.Post{
 		UserId:    user.Id,
 		ChannelId: req.ChannelId,
 		Message:   fmt.Sprintf("Meeting started at %s.", meetingURL),
 		Type:      "custom_jitsi",
 		Props: map[string]interface{}{
-			"meeting_id":        meetingID,
-			"meeting_link":      meetingURL,
-			"meeting_status":    "STARTED",
-			"meeting_personal":  false,
-			"meeting_topic":     req.Topic,
-			"from_webhook":      "true",
-			"override_username": "Jitsi",
-			"override_icon_url": "https://s3.amazonaws.com/mattermost-plugin-media/Zoom+App.png",
+			"meeting_id":              meetingID,
+			"meeting_link":            meetingURL,
+			"jwt_meeting":             JWTMeeting,
+			"jwt_meeting_valid_until": meetingLinkValidUntil.Format("2006-01-02 15:04:05 Z07:00"),
+			"meeting_personal":        false,
+			"meeting_topic":           req.Topic,
+			"from_webhook":            "true",
+			"override_username":       "Jitsi",
+			"override_icon_url":       "https://s3.amazonaws.com/mattermost-plugin-media/Zoom+App.png",
 		},
 	}
 
 	if _, err := p.API.CreatePost(post); err != nil {
 		http.Error(w, err.Error(), err.StatusCode)
 		return
-	} else {
-		err = p.API.KVSet(fmt.Sprintf("%v%v", POST_MEETING_KEY, meetingID), []byte(post.Id))
-		if err != nil {
-			http.Error(w, err.Error(), err.StatusCode)
-			return
-		}
+	}
+
+	err = p.API.KVSet(fmt.Sprintf("%v%v", POST_MEETING_KEY, meetingID), []byte(post.Id))
+	if err != nil {
+		http.Error(w, err.Error(), err.StatusCode)
+		return
 	}
 
 	w.Write([]byte(fmt.Sprintf("%v", meetingID)))
