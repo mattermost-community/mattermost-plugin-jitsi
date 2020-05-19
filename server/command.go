0a214d66ca3554e09ff5b53f0a02a966771bd264
Jason Blais <13119842+jasonblais@users.noreply.github.com>
2020-05-19 17:47:35 -0400

    Rename "/meet" command to "/jitsi"
    If a server has more than one vide plugin enabled, it may not be clear which command `/meet` applies to.

--- server/command.go
+++ server/command.go
@@ -8,7 +8,7 @@ import (
 	"github.com/mattermost/mattermost-server/v5/plugin"
 )
 
-const jitsiCommand = "meet"
+const jitsiCommand = "jitsi"
 
 func commandError(channelId string, detailedError string) (*model.CommandResponse, *model.AppError) {
 	return &model.CommandResponse{
