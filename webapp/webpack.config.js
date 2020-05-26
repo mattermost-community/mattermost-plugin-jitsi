3012339ca954cadd17578d720ffaa33c4f657e43
Jesús Espino <jespinog@gmail.com>
2020-05-26 14:42:53 +0200

    Migrate to use the same webpack/packages.json from starter-template

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -1,4 +1,3 @@
-
 var path = require('path');
 
 module.exports = {
@@ -9,36 +8,40 @@ module.exports = {
         modules: [
             'src',
             'node_modules',
-            path.resolve(__dirname)
         ],
-        extensions: ['*', '.js', '.jsx']
+        extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
     },
     module: {
         rules: [
             {
-                test: /\.(js|jsx)$/,
+                test: /\.(js|jsx|ts|tsx)$/,
                 exclude: /node_modules/,
                 use: {
                     loader: 'babel-loader',
                     options: {
-                        presets: ['env', 'react'],
+                        presets: ['@babel/env', '@babel/react'],
                         plugins: [
-                            'transform-class-properties',
-                            'transform-object-rest-spread'
-                        ]
-                    }
-                }
-            }
-        ]
+                            '@babel/plugin-proposal-class-properties',
+                            '@babel/plugin-proposal-object-rest-spread'
+                        ],
+                        cacheDirectory: true,
+
+                        // Babel configuration is in babel.config.js because jest requires it to be there.
+                    },
+                },
+            },
+        ],
     },
     externals: {
         react: 'React',
         redux: 'Redux',
-        'react-redux': 'ReactRedux'
+        'react-redux': 'ReactRedux',
+        'prop-types': 'PropTypes',
+        'react-bootstrap': 'ReactBootstrap',
     },
     output: {
         path: path.join(__dirname, '/dist'),
         publicPath: '/',
-        filename: 'main.js'
-    }
+        filename: 'main.js',
+    },
 };
