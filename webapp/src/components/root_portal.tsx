a347a9d2e64450dafdec34a0b9c35511c5e48d79
Jesús Espino <jespinog@gmail.com>
2020-05-29 21:35:24 +0200

    Adding the root portal usage to the plugin

---
+++ webapp/src/components/root_portal.tsx
@@ -0,0 +1,41 @@
+// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// See LICENSE.txt for license information.
+
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+import {Provider} from 'react-redux';
+
+import Conference from './conference/';
+
+type Props = {
+    children: React.ReactNode,
+}
+
+export default class RootPortal {
+    el: HTMLElement;
+    store: any;
+
+    constructor(store: any) {
+        this.el = document.createElement('div');
+        this.store = store;
+        const rootPortal = document.getElementById('root-portal');
+        if (rootPortal) {
+            rootPortal.appendChild(this.el);
+        }
+    }
+
+    cleanup() {
+        const rootPortal = document.getElementById('root-portal');
+        if (rootPortal) {
+            rootPortal.removeChild(this.el);
+        }
+    }
+
+    render() {
+        ReactDOM.render((
+            <Provider store={this.store}>
+                <Conference/>
+            </Provider>
+        ), this.el);
+    }
+}
