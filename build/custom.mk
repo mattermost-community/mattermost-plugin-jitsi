1ef12140a025a41f367b63c31a77d093a60b3df1
Jesús Espino <jespinog@gmail.com>
2020-06-09 15:21:12 +0200

    Adding a explanation of how to add a new language

--- build/custom.mk
+++ build/custom.mk
@@ -6,6 +6,7 @@ i18n-extract-server:
 	@goi18n merge -format json -outdir assets/i18n/ assets/i18n/active.*.json
 	@echo "Please update your assets/i18n/translate.*.json files and execute \"make i18n-merge-server\""
 	@echo "If you don't want to change any locale file, simply remove the assets/i18n/translate.??.json file before calling \"make i18n-merge-server\""
+	@echo "If you want to add a new language (for example french), simple run \"touch assets/i18n/active.fr.json\" and then run the \"make i18n-extract-server\" again"
 
 i18n-merge-server:
 	@goi18n merge -format json -outdir assets/i18n/ assets/i18n/active.*.json assets/i18n/translate.*.json
