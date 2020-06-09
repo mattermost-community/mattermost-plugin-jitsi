f9716a0fba68c1c8e0dd7beb271c37440854cf74
Jesús Espino <jespinog@gmail.com>
2020-06-09 15:16:40 +0200

    Small improvments in the i18n scripts

--- build/custom.mk
+++ build/custom.mk
@@ -1,11 +1,14 @@
 # Include custom targets and environment variables here
 
 i18n-extract-server:
-	goi18n extract -format json -outdir assets/i18n/ server/
-	for x in assets/i18n/active.*.json; do echo $$x | sed 's/active/translate/' | sed 's/^/touch /' | bash; done
-	goi18n merge -format json -outdir assets/i18n/ assets/i18n/active.*.json
+	@goi18n extract -format json -outdir assets/i18n/ server/
+	@for x in assets/i18n/active.*.json; do echo $$x | sed 's/active/translate/' | sed 's/^/touch /' | bash; done
+	@goi18n merge -format json -outdir assets/i18n/ assets/i18n/active.*.json
+	@echo "Please update your assets/i18n/translate.*.json files and execute \"make i18n-merge-server\""
+	@echo "If you don't want to change any locale file, simply remove the assets/i18n/translate.??.json file before calling \"make i18n-merge-server\""
 
 i18n-merge-server:
-	goi18n merge -format json -outdir assets/i18n/ assets/i18n/active.*.json assets/i18n/translate.*.json
-	rm assets/i18n/translate.*.json
+	@goi18n merge -format json -outdir assets/i18n/ assets/i18n/active.*.json assets/i18n/translate.*.json
+	@rm -f assets/i18n/translate.*.json
+	@echo "Translations merged, please verify your "git diff" before you submit the changes"
 
