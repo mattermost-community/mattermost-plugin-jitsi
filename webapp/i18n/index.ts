44ab4039f70563ccdb63cce68c7f8ffaa3a736a8
Extazx2 <touin.marcantoine@gmail.com>
2020-06-12 19:41:46 +0200

    French Translation for server and webapp (#106)
    * :globe_with_meridians: french webapp translation
    
    * :globe_with_meridians: french server translation
    
    * :pencil2: typos with french webapp translation
    
    * Adding new locale to the script that initilize them
    
    Co-authored-by: marc-antoine touin <marc-antoine.touin@liksi.fr>
    Co-authored-by: Jesús Espino <jespinog@gmail.com>

--- webapp/i18n/index.ts
+++ webapp/i18n/index.ts
@@ -1,8 +1,11 @@
 import * as en from './en.json';
 import * as es from './es.json';
+import * as fr from './fr.json';
 
 export function getTranslations(locale: string): {[key: string]: string} {
     switch (locale) {
+    case 'fr':
+        return fr;
     case 'en':
         return en;
     case 'es':
