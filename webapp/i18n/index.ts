5c3f7538f9e9dc616d04eca94ed74df478af83b2
Alexey Napalkov <5304490+flynbit@users.noreply.github.com>
2020-08-06 02:47:57 +0500

    Added russian translation (#145)

--- webapp/i18n/index.ts
+++ webapp/i18n/index.ts
@@ -2,6 +2,7 @@ import * as de from './de.json';
 import * as en from './en.json';
 import * as es from './es.json';
 import * as fr from './fr.json';
+import * as ru from './ru.json';
 
 export function getTranslations(locale: string): {[key: string]: string} {
     switch (locale) {
@@ -13,6 +14,8 @@ export function getTranslations(locale: string): {[key: string]: string} {
         return en;
     case 'es':
         return es;
+    case 'ru':
+        return ru;
     }
     return {};
 }
