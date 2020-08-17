2b250449930035c27be3e5a421fad27b310bb58c
Ben Schumacher <ben.schumacher@mattermost.com>
2020-08-17 12:19:34 +0200

    All all locals to getTranslations

--- webapp/i18n/index.ts
+++ webapp/i18n/index.ts
@@ -2,20 +2,53 @@ import * as de from './de.json';
 import * as en from './en.json';
 import * as es from './es.json';
 import * as fr from './fr.json';
+import * as it from './it.json';
+import * as ja from './ja.json';
+import * as ko from './ko.json';
+import * as nl from './nl.json';
+import * as pl from './pl.json';
+import * as pt_BR from './pt_BR.json';
+import * as ro from './ro.json';
 import * as ru from './ru.json';
+import * as tr from './tr.json';
+import * as uk from './uk.json';
+import * as zh_Hans from './zh_Hans.json';
+import * as zh_Hant from './zh_Hant.json';
 
 export function getTranslations(locale: string): {[key: string]: string} {
     switch (locale) {
     case 'de':
         return de;
-    case 'fr':
-        return fr;
     case 'en':
         return en;
     case 'es':
         return es;
+    case 'fr':
+        return fr;
+    case 'it':
+        return it;
+    case 'ja':
+        return ja;
+    case 'ko':
+        return ko;
+    case 'nl':
+        return nl;
+    case 'pl':
+        return pl;
+    case 'pt-BR':
+        return pt_BR;
+    case 'ro':
+        return ro;
     case 'ru':
         return ru;
+    case 'tr':
+        return tr;
+    case 'uk':
+        return uk;
+    case 'zh-CN':
+        return zh_Hans;
+    case 'zh-TW':
+        return zh_Hant;
     }
     return {};
 }
