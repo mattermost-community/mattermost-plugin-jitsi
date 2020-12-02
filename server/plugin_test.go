c6b2c62d36b05b6f8e2406116b712f18ac826728
Jesús Espino <jespinog@gmail.com>
2020-12-02 12:12:43 +0100

    Adding some unit tests to increase the coverage (#113)
    * Adding some testing arround JWT signing and verification
    
    * Adding more tests to the server
    
    * Fixing check-style

---
+++ server/plugin_test.go
@@ -0,0 +1,121 @@
+package main
+
+import (
+	"encoding/json"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/cristalhq/jwt/v2"
+	"github.com/mattermost/mattermost-server/v5/model"
+	"github.com/mattermost/mattermost-server/v5/plugin/plugintest"
+	"github.com/stretchr/testify/mock"
+	"github.com/stretchr/testify/require"
+)
+
+func TestVerifyJwt(t *testing.T) {
+	testToken := "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyfQ.mpHl842O7xEZjgQ8CyX8xYLDoEORGVMnAxULkW-u8Ek"
+	t.Run("bad secret should fail", func(t *testing.T) {
+		claims, err := verifyJwt("bad-secret", testToken)
+		require.NotNil(t, err)
+		require.Nil(t, claims)
+	})
+	t.Run("right secret should work", func(t *testing.T) {
+		claims, err := verifyJwt("test-secret", testToken)
+		require.Nil(t, err)
+		require.Equal(t, "1234567890", claims.Subject)
+		issuedAt, err := claims.IssuedAt.MarshalJSON()
+		require.Nil(t, err)
+		require.Equal(t, "1516239022", string(issuedAt))
+	})
+}
+
+func TestSignClaims(t *testing.T) {
+	claims := Claims{}
+	claims.Issuer = "test"
+	claims.Audience = []string{"test-app-id"}
+	claims.ExpiresAt = jwt.NewNumericDate(time.Time{})
+	claims.Subject = "test"
+	claims.Room = "test"
+
+	token, err := signClaims("test-secret", &claims)
+	require.Nil(t, err)
+
+	newClaims, err := verifyJwt("test-secret", token)
+	require.Nil(t, err)
+	require.Equal(t, &claims, newClaims)
+}
+
+func TestStartMeeting(t *testing.T) {
+	p := Plugin{
+		configuration: &configuration{
+			JitsiURL: "http://test",
+		},
+	}
+	apiMock := plugintest.API{}
+	defer apiMock.AssertExpectations(t)
+	apiMock.On("GetBundlePath").Return("..", nil)
+	config := model.Config{}
+	config.SetDefaults()
+	apiMock.On("GetConfig").Return(&config, nil)
+
+	p.SetAPI(&apiMock)
+
+	i18nBundle, err := p.initI18nBundle()
+	require.Nil(t, err)
+	p.i18nBundle = i18nBundle
+
+	testUser := model.User{Id: "test-id", Username: "test-username", FirstName: "test-first-name", LastName: "test-last-name", Nickname: "test-nickname"}
+	testChannel := model.Channel{Id: "test-id", Type: model.CHANNEL_DIRECT, Name: "test-name", DisplayName: "test-display-name"}
+
+	t.Run("start meeting without topic or id", func(t *testing.T) {
+		apiMock.On("CreatePost", mock.MatchedBy(func(post *model.Post) bool {
+			return strings.HasPrefix(post.Props["meeting_link"].(string), "http://test/")
+		})).Return(&model.Post{}, nil)
+		b, _ := json.Marshal(UserConfig{Embedded: false, NamingScheme: "mattermost"})
+		apiMock.On("KVGet", "config_test-id", mock.Anything).Return(b, nil)
+
+		meetingID, err := p.startMeeting(&testUser, &testChannel, "", "", false)
+		require.Nil(t, err)
+		require.Regexp(t, "^test-username-", meetingID)
+	})
+
+	t.Run("start meeting with topic and without id", func(t *testing.T) {
+		apiMock.On("CreatePost", mock.MatchedBy(func(post *model.Post) bool {
+			return strings.HasPrefix(post.Props["meeting_link"].(string), "http://test/")
+		})).Return(&model.Post{}, nil)
+		b, _ := json.Marshal(UserConfig{Embedded: false, NamingScheme: "mattermost"})
+		apiMock.On("KVGet", "config_test-id", mock.Anything).Return(b, nil)
+
+		meetingID, err := p.startMeeting(&testUser, &testChannel, "", "Test topic", false)
+		require.Nil(t, err)
+		require.Regexp(t, "^Test-topic-", meetingID)
+	})
+
+	t.Run("start meeting without topic and with id", func(t *testing.T) {
+		apiMock.On("CreatePost", mock.MatchedBy(func(post *model.Post) bool {
+			return strings.HasPrefix(post.Props["meeting_link"].(string), "http://test/")
+		})).Return(&model.Post{}, nil)
+		b, _ := json.Marshal(UserConfig{Embedded: false, NamingScheme: "mattermost"})
+		apiMock.On("KVGet", "config_test-id", mock.Anything).Return(b, nil)
+
+		meetingID, err := p.startMeeting(&testUser, &testChannel, "test-id", "", false)
+		require.Nil(t, err)
+		require.Regexp(t, "^test-username-", meetingID)
+	})
+
+	t.Run("start meeting with topic and id", func(t *testing.T) {
+		testUser := model.User{Id: "test-id", Username: "test-username", FirstName: "test-first-name", LastName: "test-last-name", Nickname: "test-nickname"}
+		testChannel := model.Channel{Id: "test-id", Type: model.CHANNEL_OPEN, TeamId: "test-team-id", Name: "test-name", DisplayName: "test-display-name"}
+
+		apiMock.On("CreatePost", mock.MatchedBy(func(post *model.Post) bool {
+			return strings.HasPrefix(post.Props["meeting_link"].(string), "http://test/")
+		})).Return(&model.Post{}, nil)
+		b, _ := json.Marshal(UserConfig{Embedded: false, NamingScheme: "mattermost"})
+		apiMock.On("KVGet", "config_test-id", mock.Anything).Return(b, nil)
+
+		meetingID, err := p.startMeeting(&testUser, &testChannel, "test-id", "Test topic", false)
+		require.Nil(t, err)
+		require.Equal(t, "test-id", meetingID)
+	})
+}
