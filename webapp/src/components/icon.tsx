f936f51c857a82e51d775584b8306eca84ac94f0
root <raju.s@demansol.com>
2021-06-15 10:00:12 +0000

    fixed linting issue

--- webapp/src/components/icon.tsx
+++ webapp/src/components/icon.tsx
@@ -1,5 +1,4 @@
 import * as React from 'react';
-import { IntlProvider, FormattedMessage } from 'react-intl';
 import Svgs from '../constants/svgs';
 
 export default class Icon extends React.PureComponent {
@@ -10,7 +9,7 @@ export default class Icon extends React.PureComponent {
                 style={style.iconStyle}
                 className='icon'
                 aria-hidden='true'
-                dangerouslySetInnerHTML={{ __html: Svgs.VIDEO_CAMERA }}
+                dangerouslySetInnerHTML={{__html: Svgs.VIDEO_CAMERA}}
             />
         );
     }
