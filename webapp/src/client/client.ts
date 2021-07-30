b0446b8a97265b57208be17589574c28632df918
Sanjay <sanjaymj@12345>
2021-07-30 10:38:49 +0530

    Fixes #141

--- webapp/src/client/client.ts
+++ webapp/src/client/client.ts
@@ -5,24 +5,32 @@ import {id} from '../manifest';
 
 export default class Client {
     url: string;
+    baseUrl: Promise<any> | string = '';
 
     constructor() {
         this.url = '/plugins/' + id;
-        if ((window as any).basename) {
-            this.url = (window as any).basename + '/plugins/' + id;
-        }
+        this.getBaseURL();
+    }
+
+    getBaseURL = async () => {
+        this.baseUrl = this.baseUrl === '' ? Client4.getConfig().then((config) => config.ServiceSettings.SiteURL) : this.baseUrl;
+        return this.baseUrl;
     }
 
     startMeeting = async (channelId: string, personal: boolean = false, topic: string = '', meetingId: string = '') => {
-        return this.doPost(`${this.url}/api/v1/meetings`, {channel_id: channelId, personal, topic, meeting_id: meetingId});
+        return this.doPost(`${await this.getBaseURL()}${this.url}/api/v1/meetings`, {channel_id: channelId, personal, topic, meeting_id: meetingId});
     }
 
     enrichMeetingJwt = async (meetingJwt: string) => {
-        return this.doPost(`${this.url}/api/v1/meetings/enrich`, {jwt: meetingJwt});
+        return this.doPost(`${await this.getBaseURL()}${this.url}/api/v1/meetings/enrich`, {jwt: meetingJwt});
+    }
+
+    setUserStatus = async (userId: string | null, status: string) => {
+        return this.doPut(`${await this.getBaseURL()}${this.baseUrl}/api/v4/users/${userId}/status`, {user_id: userId, status});
     }
 
     loadConfig = async () => {
-        return this.doPost(`${this.url}/api/v1/config`, {});
+        return this.doPost(`${await this.getBaseURL()}${this.url}/api/v1/config`, {});
     }
 
     doPost = async (url: string, body: any, headers: any = {}) => {
@@ -31,6 +39,30 @@ export default class Client {
             body: JSON.stringify(body),
             headers
         };
+
+        const response = await fetch(url, Client4.getOptions(options));
+
+        if (response.ok) {
+            return response.json();
+        }
+
+        const text = await response.text();
+
+        throw new ClientError(Client4.url, {
+            message: text || '',
+            status_code: response.status,
+            url
+        });
+    }
+
+    doPut = async (url: string, body: any, headers: any = {}) => {
+        const options = {
+            method: 'put',
+            body: JSON.stringify(body),
+            headers
+        };
+
+        options.headers['X-Requested-With'] = 'XMLHttpRequest';
 
         const response = await fetch(url, Client4.getOptions(options));
 
