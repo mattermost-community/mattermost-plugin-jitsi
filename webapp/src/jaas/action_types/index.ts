4dae89dc3167c0627788d2670cabc282ac218710
raghavaggarwal2308 <raghav.aggarwal@brightscout.com>
2022-06-27 15:53:08 +0530

    [MI-1865]Removed redundant jaas directory

--- webapp/src/jaas/action_types/index.ts
+++
@@ -1,4 +0,0 @@
-
-export enum JaaSActionTypes {
-    START_MEETING_WINDOW = 'jaas/START_MEETING_WINDOW'
-}
--- webapp/src/jaas/components/conference/index.ts
+++
@@ -1,4 +0,0 @@
-import {connect} from 'react-redux';
-import {JaaSConference, MapDispatchToProps} from './jaas_conference';
-
-export default connect(null, MapDispatchToProps)(JaaSConference);
