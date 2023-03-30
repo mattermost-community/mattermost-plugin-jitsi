09198b904bf2108c1ff40303961e288197788d89
Jose Peso <jmpeso@gmail.com>
2023-03-30 15:41:59 +0200

    fix rudder key and add missing arm64 architectures

--- build/custom.mk
+++ build/custom.mk
@@ -1,5 +1,11 @@
 # Include custom targets and environment variables here
 
+# If there's no MM_RUDDER_PLUGINS_PROD, add DEV data
+RUDDER_WRITE_KEY = 1d5bMvdrfWClLxgK1FvV3s4U1tg
+ifdef MM_RUDDER_PLUGINS_PROD
+	RUDDER_WRITE_KEY = $(MM_RUDDER_PLUGINS_PROD)
+endif
+
 default: all
 
 i18n-extract-server:
@@ -19,4 +25,4 @@ ifndef MM_RUDDER_WRITE_KEY
     MM_RUDDER_WRITE_KEY = 1d5bMvdrfWClLxgK1FvV3s4U1tg
 endif
 
-GO_BUILD_FLAGS += -ldflags '-X "github.com/mattermost/mattermost-plugin-api/experimental/telemetry.rudderWriteKey=$(MM_RUDDER_WRITE_KEY)"'
+GO_BUILD_FLAGS += -ldflags '-X "github.com/mattermost/mattermost-plugin-api/experimental/telemetry.rudderWriteKey=$(RUDDER_WRITE_KEY)"'
